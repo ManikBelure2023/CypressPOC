bcfa22b5d9b93dd0319b5975191d473e23ec5d34
simpletest github <manikvbelure@gmail.com>
2024-02-02 16:43:01 +0530

    my-test-cases

--- cypress/e2e/Assignment-1/test-Scenario-8.cy.js
+++ cypress/e2e/Assignment-1/test-Scenario-8.cy.js
@@ -1,47 +1,48 @@
 ///<reference types="cypress"/>
-describe('Tls search and enter text in input', () => {
-    // runs before every test block
+describe('TLS search and enter text in input', () => {
+    // Runs before every test block
     beforeEach(() => {
         try {
-            cy.visit('https://www.the-tls.co.uk/')
-        } catch {
-            cy.log("Error visiting the TLS website:")
+            cy.visit('https://www.the-tls.co.uk/');
+        } catch (error) {
+            cy.log("Error visiting the TLS website:", error);
         }
-    })
+    });
 
-    it('assert the textbox and type Mary Beard to find related articles', () => {
+    it('Assert the textbox and type "Mary Beard" to find related articles', () => {
         try {
             let value1;
             let value2;
-            cy.get(".tls-header-navigation__search-icon").click().should('be.visible')
-            cy.get(".ais-SearchBox-input[type='search']").clear().type('Mary Beard', {timeout: 5000})
-            cy.get('.tls-search-core__hits')
-                .find('li')
-                .then(($value) => {
-                    value1 = $value.length
-                    cy.log('Number of article', value1)
-                })
 
-            cy.get('.tls-show-more>').eq(1).click()
+            cy.get(".tls-header-navigation__search-icon").click().should('be.visible');
+            cy.get(".ais-SearchBox-input[type='search']").clear().type('Mary Beard', { timeout: 5000 });
+
             cy.get('.tls-search-core__hits')
                 .find('li')
                 .then(($value) => {
-                    value2 = $value.length
-                    cy.log('Number of article', value2)
-                })
+                    value1 = $value.length;
+                    cy.log('Number of articles before clicking "Show More":', value1);
+                });
+
+            cy.get('.tls-show-more>').eq(1).click();
+
+            cy.get('.tls-search-core__hits')
+                .find('li')
+                .then(($value) => {
+                    value2 = $value.length;
+                    cy.log('Number of articles after clicking "Show More":', value2);
+                });
+
             let sum = value1 + value2;
-            
-            cy.get('.tls-article-label__category a[href="Twenty Questions"]').click({ force: true })
-            expect(sum).to.equal(38)
-            // cy.get('div.tls-search-page__wrapper > :nth-child(2)').should('have.value',40)
-            
-        } catch { 
-            cy.log("error in test Scenario-8:")
+
+            // cy.get('.tls-article-label__category a[href="Twenty Questions"]').click({ force: true });
+
+            // expect(sum).to.equal(40);
+            // Alternatively, you can assert against a specific element with expected count
+            cy.get('div.tls-search-page__wrapper > :nth-child(2)').should('have.length',40);
+
+        } catch (error) {
+            cy.log("Error in test Scenario-8:", error);
         }
-    })
-})
-
-
-
-
-
+    });
+});
